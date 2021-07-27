b6327c2cca8c6ef02139d57fb080452437f44077
dionyysus <gizemcsk1@gmail.com>
2021-07-27 18:39:19 +0300

    project updated

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/**
+ * @format
+ */
+
+import {View, Navigator, StatusBar} from 'react-native';
+import React, {Component} from 'react';
+import Home from './screens/Home/Home';
+import OnBoard from './screens/OnBoard/OnBoard';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import colors from './consts/colors';
+import Cart from './screens/Cart/Cart';
+
+const Stack = createStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.dark} />
+      <Stack.Navigator screenOptions={{header: () => null}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="OnBoard" component={OnBoard} />
+        <Stack.Screen name="Cart" component={Cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
